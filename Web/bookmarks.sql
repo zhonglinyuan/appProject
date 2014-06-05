bfd95c4c8465eaadee4993470f418a698e02e29d
Kai Zou <kaizou898@gmail.com>
2014-06-05 17:17:31 -0400

    initial web app code: need to change it for our purposes

---
+++ Web/bookmarks.sql
@@ -0,0 +1,16 @@
+create database bookmarks;
+use bookmarks;
+
+create table user  (
+  username varchar(16) primary key,
+  passwd char(40) not null,
+  email varchar(100) not null
+);
+
+create table bookmark (
+  username varchar(16) not null,
+  bm_URL varchar(255) not null,
+  index (username),
+  index (bm_URL)
+);
+
--- Web/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "<html>hello world!</html>;
-?>
