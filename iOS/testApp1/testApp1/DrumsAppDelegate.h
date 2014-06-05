a057f9721260111e75c05891ff9338902b69bc3a
ZhongLin Yuan <zhonglinyuan@user3-219-239.wireless.utoronto.ca>
2014-06-04 20:46:20 -0400

    see what happens

---
+++ iOS/testApp1/testApp1/DrumsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DrumsAppDelegate.h
+//  testApp1
+//
+//  Created by ZhongLin Yuan on 2014-06-04.
+//  Copyright (c) 2014 ZhongLin Yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrumsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
